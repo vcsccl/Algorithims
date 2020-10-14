1f09b3ba947a0219630e0b5857351e5c7f49d2bd
Jon Palmer <33100226+vcsccl@users.noreply.github.com>
2020-10-14 00:13:38 -0700

    Update stoogeTime.py

--- stoogeTime.py
+++ stoogeTime.py
@@ -1,71 +1,77 @@
-""" Stooge Time - Experimental O(n) Run
+""" Stooge Time
 
     Jon Palmer
     CS 325 Fall 2020
-    Last Edit: 10/07/2020
+    Last Edit: 10/06/2020
 
     Description:   Stooge sort implementation that generates arrays of size n 
-    containing random integer values, records the time taken to sort, and
-    outputs runs/average to CSV.  
+    containing random integer values, and records the time taken to sort.  
 """
 import os 
 import time 
 import random
 
-# Generate arrays of n size and output nested list
-n1 = 50000
-n2 = 100000
-n3 = 150000
-n4 = 200000
-nIn = [[0]*n1, [0]*n2, [0]*n3, [0]*n4]
+n1 = 50
+n2 = 100
+n3 = 150
+n4 = 200
+
+nL1 = [] * n1
+nL2 = [] * n2
+nL3 = [] * n3
+nL4 = [] * n4
+
+nLi1 = [n1]
+nLi2 = [n2]
+nLi3 = [n3]
+nLi4 = [n4]
+
+nIn = [[], [], [], []]
 nOut = [[n1],[n2],[n3],[n4]]
 
-# Sort arrays using stooge sort
-def stoogeSort(inArray, l, h):
-    a = h - l + 1
-    if (a == 2) and (inArray[l] > inArray[h]):
-        t = inArray[l]
-        inArray[l] = inArray[h]
-        inArray[h] = t
-    elif a > 2: 
-        m = (int)((a) / 3)
-        stoogeSort(inArray, l, (h - m))
-        stoogeSort(inArray, l+t, (h))
-        stoogeSort(inArray, l, (h - m))
-    else:
-        return
-    return inArray
-
-# Fill and sort arrays 
-def printOut(inArray, mIt):
+def main(array, size, output):
     avgTime = 0
-    for k in range(1, 8):
-        tempTime = 0
-        for l in range(0, len(inArray)):
-            inArray[l] = random.randint(0, 10000)
-        startTime = time.time()
-        stoogeSort(inArray, 0, (len(inArray)-1))
-        tempTime += time.time() - startTime
-        nOut[mIt].insert(k, tempTime)
-        avgTime = avgTime + tempTime
-    avgTime = avgTime / 7
-    nOut[mIt].extend([avgTime])
+    for e in range(size):
+        array.append(random.randint(0, 10000))
+        print(array)
+        for axe in range(7):
+            tempTime = 0
+            startTime = time.time()
+            stoogeSort(array, 0, (len(array) - 1))
+            tempTime += time.time() - startTime
+            output.append(tempTime)
+ 
+    print(output)
 
-# Definition call
-ki = 0
-for a in nIn:
-    printOut(a, ki)
-    ki += 1
-    
-# Convert to string and output to Terminal
-inPut_out = ""
-for i in nOut: 
-    inPut_out += ", ".join(map(str, i)) + "\n"
-print(inPut_out)
+def stoogeSort(inArray, i, j):
+    n = j - i + 1
+    if (n == 2) and (inArray[i] > inArray[j]):
+        t = inArray[i]
+        inArray[i] = inArray[j]
+        inArray[j] = t
+    if n > 2: 
+        t = (int)((n) / 3)
+        stoogeSort(inArray, i, (j - t))
+        stoogeSort(inArray, i + t, (j))
+        stoogeSort(inArray, i, (j - t))
 
-# Write to File
-if os.path.exists("test.txt"):
-    os.remove("test.txt")
-writeFile = open("test.txt", "x")
-writeFile.write(inPut_out)
-writeFile.close()
+def format(rawData):
+    forData = ""
+    for i in rawData: 
+        forData += ", ".join(map(str, i)) + "\n"
+    return forData
+
+def write(forData):
+    if os.path.exists("data.txt"):
+        os.remove("data.txt")
+    writeFile = open("data.txt", "x")
+    writeFile.write(forData)
+    writeFile.close()
+
+main(nL1, n1, nLi1)
+main(nL2, n2, nLi2)
+main(nL3, n3, nLi3)
+main(nL4, n4, nLi4)
+
+forData = format(nOut)
+write(forData)
